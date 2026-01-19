c935c6ee8606c4553c8e1053de952a7f6f6077bb
Razin Abdilbarr <abdilbarrrazin@gmail.com>
2026-01-19 01:06:39 +0000

    menambah halaman tampilkan data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request
 import pymysql
 import os
 
@@ -16,7 +16,7 @@ def buat_koneksi():
 
 @app.route("/test")
 def static_test():
-	return send_file('templates/index.html')
+	return render_template('templates/index.html')
 
 
 @app.route("/")
@@ -30,6 +30,38 @@ def index():
 	#	status = "KONEKSI BERHASIL"
 
 		return render_template("index.html", verses=baris)
+	
+	except pymysql.Error as e:
+		return f"Error database {e}"
+
+
+
+@app.route("/tambahdata", methods=["GET", "POST"])
+def tambah_data():
+	if request.method == "POST":
+		teksayat = request.form["ayat"]
+		terjemah = request.form["terjemah"]
+
+		conn = buat_koneksi()
+		cur = conn.cursor()
+		perintahquery = "INSERT INTO alFatihah (ayat, terjemah) VALUES (%s, %s)"
+		cur.execute(perintahquery, (teksayat, terjemah))
+		conn.commit()
+		conn.close()
+
+	return render_template("tambah_data.html")
+
+
+@app.route("/tampilkandata")
+def tampilkan_data():
+	try:
+		conn = buat_koneksi()
+		cur = conn.cursor()
+		cur.execute("SELECT * FROM alFatihah")
+		baris = cur.fetchall()
+		conn.close()
+
+		return render_template("tampilkan_data.html", verses=baris)
 	
 	except pymysql.Error as e:
 		return f"Error database {e}"
